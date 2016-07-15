ce21f7dfa7f58b8070067741324218dd29c9fbc5
Cécile Delmon <cecile.delmon@gmail.com>
2016-07-15 20:12:51 +0200

    collect tweets by geoloc

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ var Twit = require('twit');
 
 //Variables a proteger
 var client = new Twit({
-  consumer_key: '...',
-  consumer_secret: '...',
-  access_token: '...',
-  access_token_secret: '...'
+  consumer_key: 'pspb2iLZ6xz0KuApSZWOUQpkr',
+  consumer_secret: 'aUVXVyRZNSJg9TGs3JuioT9ubTR9whFg7S2PkXq1RiaI1eX8rP',
+  access_token: '714454775-GhfVvCwYLx0Be2tbBtCABsHeRbHxsupvHqq5lb22',
+  access_token_secret: '8UITh7VSbGcCxLxFoKX3QEhaEZXii6dQfSHY4niG4qsbo'
 });
 
 
-client.get('search/tweets', {q:'pokemonGo',  count: 50}, function(err,data,response) {
-  console.log(data);
-});
+client.get('search/tweets', {q:'pokemonGo',  geocode:'48.856614,2.3522219000000177,2mi'}, function(err,data,response) {
+  //console.log(data.statuses[3].place.bounding_box.coordinates[0][0]);
+  //console.log(data);
+  data.statuses.forEach(function(row) {
+    if (row.place!=null) {
+      console.log(row.place.bounding_box.coordinates[0][0]);
+      console.log(row.text);
+    };
+  });
+  });
+
 
 // stream.on('tweet', function(tweet) {
 //   });
